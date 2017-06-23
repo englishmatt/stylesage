bd2b4ee4e2527f526b8f46b331263df11fe73836
Matt Titchener <matt.titchener@stylesage.io>
2017-06-22 22:58:35 -0600

    Implement basic ASP.NET Core MVC middleware

--- src/Promo.Prelaunch.Presentation/Startup.cs
+++ src/Promo.Prelaunch.Presentation/Startup.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,26 +8,48 @@ namespace Promo.Prelaunch.Presentation
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment environment)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void ConfigureServices(IServiceCollection services)
         {
-            loggerFactory.AddConsole();
+            services.AddMvc();
+        }
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public void Configure(
+            IApplicationBuilder application,
+            IHostingEnvironment environment,
+            ILoggerFactory loggerFactory)
+        {
+            loggerFactory
+                .AddConsole(Configuration.GetSection("Logging"))
+                .AddDebug();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            application.UseStaticFiles();
+
+            // Register routes
+            application.UseMvc(routes => {
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
+
+            if (environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+            }
         }
     }
 }
